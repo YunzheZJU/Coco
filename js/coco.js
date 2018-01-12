b23417fce7e8d073345e948fd819e0ac68495911
Yunzhe <iLoveLive@honoka.moe>
2018-01-12 18:01:05 +0800

    Before enable SSAO.

--- js/coco.js
+++ js/coco.js
@@ -5,44 +5,82 @@
 'use strict';
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(-5, 20, -5);
+camera.position.set(0, 20, 0);
 
 var renderer = new THREE.WebGLRenderer({
     antialias: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor("#ffffff");
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
-//Create a PointLight and turn on shadows for the light
-var light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(5, 10, 5);
-light.castShadow = true;            // default false
-scene.add(light);
-//Set up shadow properties for the light
-light.shadow.mapSize.width = 1024;  // default
-light.shadow.mapSize.height = 1024; // default
-light.shadow.camera.near = 0.5;       // default
-light.shadow.camera.far = 500;      // default
+// //Create a PointLight and turn on shadows for the light
+// var light = new THREE.PointLight(0xffffff, 1, 100);
+// light.position.set(5, 10, 5);
+// light.castShadow = true;            // default false
+// scene.add(light);
+// //Set up shadow properties for the light
+// light.shadow.mapSize.width = 1024;  // default
+// light.shadow.mapSize.height = 1024; // default
+// light.shadow.camera.near = 0.5;       // default
+// light.shadow.camera.far = 500;      // default
 
-var geometry = new THREE.BoxGeometry(2, 0.5, 2);
-var loader = new THREE.TextureLoader();
-var material = new THREE.MeshBasicMaterial();
-material.map = loader.load("image/texture.jpg");
-var cube = new THREE.Mesh(geometry, material);
-cube.castShadow = true;
-cube.receiveShadow = false;
-scene.add(cube);
+// var geometry = new THREE.BoxGeometry(2, 0.5, 2);
+// var loader = new THREE.TextureLoader();
+// var material = new THREE.MeshStandardMaterial();
+// material.map = loader.load("image/texture.jpg");
+var loader = new THREE.OBJLoader();
+var cube;
+loader.load("model/test.obj", function (group) {
+    var loader = new THREE.TextureLoader();
+    var material = new THREE.MeshBasicMaterial();
+    material.map = loader.load( 'image/gridTexture.bmp' );
+    material.map.wrapS = THREE.RepeatWrapping;
+    group.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+            child.material = material;
+        }
+    } );
+    cube = group;
+    // object.material = material;
+    // group.position.x = - 0.45;
+    // group.rotation.y = - 0.45;
+    scene.add(group);
+    TweenLite.to(cube.rotation, 2, {
+        z: -Math.PI * 1.1,
+        delay: 1,
+        ease: Quad.easeInOut,
+        onComplete: function () {
+            TweenLite.to(cube.rotation, 0.4, {
+                z: -Math.PI * 0.9,
+                onComplete: function () {
+                    TweenLite.to(cube.rotation, 0.2, {
+                        z: -Math.PI * 1.05,
+                        onComplete: function () {
+                            TweenLite.to(cube.rotation, 0.1, {
+                                z: -Math.PI
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+// var cube = new THREE.Mesh(geometry, material);
+// // cube.castShadow = true;
+// // cube.receiveShadow = false;
+// scene.add(cube);
 
-//Create a plane that receives shadows (but does not cast them)
-var planeGeometry = new THREE.BoxGeometry(10, 0.1, 10);
-var planeMaterial = new THREE.MeshStandardMaterial({color: 0x00ff00});
-var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.position.set(0, -5, 0);
-plane.receiveShadow = true;
-scene.add(plane);
+// //Create a plane that receives shadows (but does not cast them)
+// var planeGeometry = new THREE.BoxGeometry(10, 0.1, 10);
+// var planeMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+// var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// // plane.position.set(0, -5, 0);
+// // plane.receiveShadow = true;
+// scene.add(plane);
 
 function animate() {
     requestAnimationFrame(animate);
@@ -65,14 +103,14 @@ function rotateAroundObjectAxis(object, axis, radians) {
     object.rotation.setFromRotationMatrix(object.matrix);
 }
 
-var rotationRadius = {cube_0: 0};
-var tween_1 = TweenLite.to(rotationRadius, 2 * 60, {
-    cube_0: 4 * 90 / 180 * Math.PI / (2 * 60),
-    delay: 60,
-    onUpdate: function () {
-        cube.rotateZ(rotationRadius.cube_0);
-        // rotateAroundObjectAxis(cube, new THREE.Vector3(0, 0, 1), rotationRadius.cube_0);
-    },
-    ease: Quad.easeInOut,
-    useFrames: true
-});
+var rotationRadius = {cube_0: 0.1};
+// var tween_1 = TweenLite.to(rotationRadius, 2 * 60, {
+//     cube_0: -4 * 90 / 180 * Math.PI / (2 * 60),
+//     delay: 60,
+//     onUpdate: function () {
+//         cube.rotateZ(rotationRadius.cube_0);
+//         // rotateAroundObjectAxis(cube, new THREE.Vector3(0, 0, 1), rotationRadius.cube_0);
+//     },
+//     ease: Quad.easeInOut,
+//     useFrames: true
+// });
---
+++ js/model.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Yunzhe on 2018/1/12.
+ */
+
+'use strict';
+
+class grid {
+    constructor(num, position, type) {
+        this.num = num;
+        this.position = position;
+        this.type = type;
+        this.cube = null;
+        this.isRotated = false;
+        this.material = new THREE.MeshBasicMaterial();
+    }
+
+
+}
